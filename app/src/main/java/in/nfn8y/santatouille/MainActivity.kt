acf639ccb65ef6eae77791d465dad6860bc9bc61
Yash <yashwank@thoughtworks.com>
2018-04-22 00:22:28 +0530

    [Yash]: Login and Signup pages created

--- app/src/main/java/in/nfn8y/santatouille/MainActivity.kt
+++ app/src/main/java/in/nfn8y/santatouille/MainActivity.kt
@@ -1,12 +1,31 @@
 package `in`.nfn8y.santatouille
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean{
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val itemId = item?.itemId
+        if (itemId == R.id.action_settings) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
